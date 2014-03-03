0ef02fdea2a4e9e666489e5daf131fdff679f37b
xtlx2000 <lvpengcheng6300@gmail.com>
2014-03-03 22:49:45 +0800

    Signed-off-by: xtlx2000 <lvpengcheng6300@gmail.com>

--- _2.1.1从已排序数组中删除重复元素.cpp
+++ _2.1.1从已排序数组中删除重复元素.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
+void function(int *a, int num)
+{
+    int index = 0;
+    for(int i = 1; i < num; i++){
+        if(a[index] == a[i]){
+            //++i; skip
+        }else{
+            a[++index] = a[i];
+        }
+    }
+    cout <<"num="<<index+1<<endl;
+
+    cout <<"list: ";
+    for(int i = 0; i < index; i++){
+        cout <<a[i];
+    }
+    cout <<endl;
+}
 
 int main()
 {
+    int a[10] = {1,1,2,2,2,3,4,5,6,7};
+    function(a, 10);
 }
