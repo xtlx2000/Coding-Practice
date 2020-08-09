2ed81b40d3db88ef2ef4dda9d34880680028f163
xtlx2000 <lvpengcheng6300@gmail.com>
2020-08-09 11:53:54 +0100

    modify 093

--- leetcode/093.cpp
+++ leetcode/093.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Solution {
     //input
     std::string mStr;
@@ -8,13 +14,13 @@ class Solution {
 
     void helper(int level)
     {
-        if (level > 4)
+        if (level == 3)
         {
-            mResult.push_back(mCurrIP);
+            mResult.push_back(mCurrIP + "." + mPostfix);
             return;
         }
 
-        for (size_t i = 0; i < mPostfix.size()-1; ++i)
+        for (size_t i = 1; i < mPostfix.size(); ++i)
         {
             //push
             std::string oldPostfix = mPostfix;
@@ -30,12 +36,23 @@ class Solution {
             //dfs
             if (level == 2 && stoi(mPostfix) > 255)
             {
-                break;
+                // continue
             }
-            helper(level+1);
+            else
+            {
+                helper(level+1);
+            }
             //pop
             mPostfix = oldPostfix;
             mCurrIP = oldCurrIP;
+        }
+    }
+
+    void trimResult()
+    {
+        for (size_t i = 0; i < mResult.size(); ++i)
+        {
+            mResult[i] = mResult[i].substr(1);
         }
     }
 
@@ -44,6 +61,19 @@ public:
         mStr = s;
         mPostfix = mStr;
         helper(0);
+        trimResult();
         return mResult;
     }
 };
+
+int main()
+{
+    std::string s = "19216811";
+    Solution ss;
+    vector<string> r = ss.restoreIpAddresses(s);
+    for (int i = 0; i < r.size(); ++i)
+    {
+        std::cout <<r[i]<< std::endl;
+    }
+    return 0;
+}
